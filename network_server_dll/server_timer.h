6a097f6111529928606cd9fd57126acca10fc5df
Roman Ivanov <romanivanov21@mail.ru>
2015-02-11 00:12:42 +0300

    Добавлен server_timer.h

---
+++ network_server_dll/server_timer.h
@@ -0,0 +1,29 @@
+/*******************************************************************
+* Файл: server_timer.h											   *
+* Описание:содержит обяъявление функций таймера					   *
+* Автор: Иванов Роман Витальевич (c)							   *
+* Год: 2014														   *
+*																   *
+********************************************************************/
+
+#ifndef _SERVER_TIMER_
+#define _SERVER_TIMER_
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+
+
+namespace server_netw
+{
+	class server_time : private boost::noncopyable
+	{
+	public:
+		std::string current_date_time_string();
+		void timer_start();
+		boost::int64_t timer_stop();
+	private:
+		boost::posix_time::ptime start_time_;
+		boost::posix_time::ptime stop_time_;
+	};
+}
+#endif
